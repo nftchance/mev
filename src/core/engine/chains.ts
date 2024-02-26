ad80f689fce90d572d03c3345fe9fb5d496eed02
nftchance <chance@utc24.io>
2024-02-25 18:53:31 -0600

    chore: better building of provided configuration

---
+++ src/core/engine/chains.ts
@@ -0,0 +1,5 @@
+export const mainnet = 1
+export const optimism = 10
+export const polygon = 137
+export const base = 8453
+export const arbitrum = 42161
--- src/core/engine/config.ts
+++ src/core/engine/config.ts
@@ -1,10 +1,46 @@
 import dotenv from "dotenv"
 
-import { DEFAULT_NETWORKS } from "@/core/engine/constants"
+import {
+    DEFAULT_NETWORK_CONFIG,
+    DEFAULT_NETWORK_REFERENCES,
+    DEFAULT_NETWORKS,
+} from "@/core/engine/constants"
+import { logger } from "@/lib/logger"
 import { Config, Network } from "@/lib/types/config"
 
 dotenv.config()
 
-export const defineConfig = (
-    config: Record<keyof typeof DEFAULT_NETWORKS, Network> = DEFAULT_NETWORKS
-): Config => config
+export const defineConfig = (networks: Partial<Config>): Config => {
+    const config: Config = {}
+
+    // * While we have a set of default networks, the configuration only
+    //   contains the chains that the user has actually configured.
+    for (const networkId in networks) {
+        const network: Network = {
+            // * Append the default values to the network so that the user
+            //   can operate with the default RPC and Etherscan URLs when
+            //   they are not provided.
+            ...DEFAULT_NETWORKS[networkId],
+            // * Set the default values of the references for the network.
+            // ! Realistically, most implementations of the Engine configuration
+            //   will have at least some amount of contract references, but
+            //   we can't assume that they will be provided at time of loading.
+            ...DEFAULT_NETWORK_REFERENCES,
+            // * Set the operational pieces of the Engine as null
+            //   and then overwrite them with the provided values
+            //   when they are not undefined.
+            ...DEFAULT_NETWORK_CONFIG,
+            // * Finally set all the fields that the user provided in
+            //   their configuration instantiation.
+            ...networks[networkId],
+        }
+
+        config[networkId] = network
+    }
+
+    if (Object.keys(config).length === 0) {
+        logger.warn("No networks were configured.")
+    }
+
+    return config
+}
